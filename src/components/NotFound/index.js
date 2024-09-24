a5921c5f636822d2f7bd6a25d687b8fd2c942fbf
venky3553 <sivaprasad111222333@gmail.com>
2024-09-24 17:54:16 +0000

    Solution

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,9 @@
+// Write your code here
+const NotFound = () => {
+  return (
+    <div className="notfound-container">
+      <h1 className="error-msg">Page Not Found</h1>
+    </div>
+  )
+}
+export default NotFound
